789f4e8ef75f770ae075f7e5e5cf4e00c77a4b78
maximsect <pocketmaxim2+1@gmail.com>
2025-08-02 12:25:14 +0900

    anim更新 2025/8/2

--- Assets/scripts/presentmaker.cs
+++ Assets/scripts/presentmaker.cs
@@ -5,6 +5,8 @@ public class presentmaker : MonoBehaviour
 
     readonly float interval=10f;
     public GameObject newPrefab;
+    float area_x = 9f;
+    float area_y = 12f;
     
     void Start()
     {
@@ -18,12 +20,10 @@ public class presentmaker : MonoBehaviour
 
     void CreatePrefab()
     {
-        float area_x = 9f;
-        float area_y = 15f;
 
         Vector3 newPos = transform.position;
-        newPos.x = UnityEngine.Random.Range(-area_x / 2, area_x / 2);
-        newPos.y = UnityEngine.Random.Range(-area_y / 2, area_y / 2);
+        newPos.x = UnityEngine.Random.Range(-this.area_x / 2, this.area_x / 2);
+        newPos.y = UnityEngine.Random.Range(-this.area_y / 2 + 2, this.area_y / 2 + 2);
         newPos.z = 0f;
 
         GameObject newGameObject = Instantiate(newPrefab) as GameObject;

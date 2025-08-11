dc191cefb2439bf4c3b7d66dbef36f68d0481a26
Cyferno1 <falconzz.mh@gmail.com>
2025-08-11 11:37:20 +0900

    猫鬼パラメータ誤植修正　明かりの追加

--- Assets/Scripts/Hibana_Script/BackGround.cs
+++ Assets/Scripts/Hibana_Script/BackGround.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class BackGround : MonoBehaviour
 {
+
+    public float speeds = 0.3f;
     void Update()
     {
-        transform.position = Camera.main.gameObject.transform.position * 0.3f;
+        transform.position = Camera.main.gameObject.transform.position * speeds;
     }
 }

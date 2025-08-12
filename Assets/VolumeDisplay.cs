9aaa42a903448d418240f8c5dca16a0eb9011f55
Hiroki <shibata.hiroki.1216@gmail.com>
2025-08-12 16:08:42 +0900

    BGM

--- Assets/VolumeDisplay.cs
+++ Assets/VolumeDisplay.cs
@@ -5,7 +5,7 @@ public class VolumeDisplay : MonoBehaviour
 {
         public void ChangeVolume(float volume)
     {
-        float soundVol = Mathf.Lerp(0, 100, 1 + volume / 80);
+        float soundVol = Mathf.Lerp(0, 100, 0.8f + volume / 50);
         GetComponent<TextMeshProUGUI>().text = ((int)soundVol).ToString();
     }
 }
